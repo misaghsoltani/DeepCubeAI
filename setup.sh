0141317dc41434ca4468e07674d68e0433c25e2e
Misagh <misaghsoltani@gmail.com>
2024-08-14 13:20:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-unset PYTHONPATH
-
-DIR=`pwd`
+DIR=$(pwd)
 export PYTHONPATH=$DIR:$PYTHONPATH
-export PYTHONPATH=$DIR"/puzzlegen":$PYTHONPATH
 
-export RL_ENV_DATA="../../../rl_env_data/"
-#export RL_ENV_DATA=$DIR"/data"
+export RL_ENV_DATA="../../rl_env_data/"
