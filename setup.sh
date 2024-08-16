2c143ff26136ff707f466ec00e59965a9d57cfdd
Misagh <misaghsoltani@gmail.com>
2024-08-15 20:09:25 -0400

    Update setup.sh to ensure DCAI_DIR is correctly set in PYTHONPATH and update RL_ENV_DATA path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
-DIR=$(dirname "$(realpath "$0")")
-export PYTHONPATH=$DIR:$PYTHONPATH
+DCAI_DIR=$(dirname "$(realpath "$0")")
 
-export RL_ENV_DATA="../../rl_env_data/"
+# Check if $DCAI_DIR is already at the beginning of PYTHONPATH
+if [[ "$PYTHONPATH" != "$DCAI_DIR"* ]]; then
+    # Remove $DCAI_DIR from PYTHONPATH if it exists anywhere
+    PYTHONPATH=$(echo "$PYTHONPATH" | sed -e "s|^$DCAI_DIR:*||" -e "s|:$DCAI_DIR||")
+
+    # Add $DCAI_DIR to the beginning of PYTHONPATH
+    export PYTHONPATH="$DCAI_DIR:$PYTHONPATH"
+fi
+
+export RL_ENV_DATA="$DCAI_DIR/data/sokoban_env_data/"
